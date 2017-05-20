125cbf28cbdf2afc210c2a6250824a3025a8a73f
fengzi <fengzi8808010@163.com>
2017-05-20 09:54:18 +0800

    个人博客首页初稿

--- script/index.js
+++ script/index.js
@@ -2,9 +2,9 @@
 * @Author: fengzi
 * @Date:   2017-05-19 22:24:36
 * @Last Modified by:   fengzi
-* @Last Modified time: 2017-05-19 22:24:40
+* @Last Modified time: 2017-05-19 22:46:00
 */
 
 'use strict';
 
-alert(1)
+//TODO
